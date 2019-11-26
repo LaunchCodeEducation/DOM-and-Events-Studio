8363ac641529e0c48444b8daad46459175db6c3b
talljessica <jrnewport07@gmail.com>
2019-11-26 11:41:43 -0600

    after take off changed background color to blue and shuttle height to 10000. added event handlers to landing button, and mission abort button

--- scripts.js
+++ scripts.js
@@ -1,20 +1,33 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 function init(){
-    let flightStatus = document.getElementById("flightStatus")
-    let shuttleBackground = document.getElementById("shuttleBackground")
-    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight")
-    let takeoffButton = document.getElementById("takeoff")
-    let landingButton = document.getElementById("landing")
-    let missionAbortButton = document.getElementById("missionAbort")
-    let upButton = document.getElementById("Up")
-    let downButton = document.getElementById("Down")
-    let rightButton = document.getElementById("Right")
-    let leftButton = document.getElementById("Left")
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    let takeoffButton = document.getElementById("takeoff");
+    let landingButton = document.getElementById("landing");
+    let missionAbortButton = document.getElementById("missionAbort");
+    let upButton = document.getElementById("Up");
+    let downButton = document.getElementById("Down");
+    let rightButton = document.getElementById("Right");
+    let leftButton = document.getElementById("Left");
+    let rocket = document.getElementById("rocket");
     takeOffButton.addEventListener("click",function(){
         if(window.confirm(("Confirm that the shuttle is ready for takeoff."))===true){
         flightStatus.innerHTML= "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = "blue";
+        spaceShuttleHeight.innerHTML = 10000;
         }
     });
+    landingButton.addEventListener("click", function(){
+        if((window.confirm("The shuttle is landing. Landing gear engaged."))=== true){
+            flightStatus.innerHTML= "The shuttle has landed.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = 0;
+        }
+    });
+    missionAbortButton.addEventListener("click", function(){
+        if(9window.confirm(""))
+    })
 }
   window.onload = init;
