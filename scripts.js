d3f7e95ff37279c3dd303d6a68093eb67a078746
talljessica <jrnewport07@gmail.com>
2019-11-26 09:44:13 -0600

    Added confirmation window to take off button, and updated flight status

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init(){
+    let flightStatus = document.getElementById("flightStatus")
+    let shuttleBackground = document.getElementById("shuttleBackground")
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight")
+    let takeoffButton = document.getElementById("takeoff")
+    let landingButton = document.getElementById("landing")
+    let missionAbortButton = document.getElementById("missionAbort")
+    let upButton = document.getElementById("Up")
+    let downButton = document.getElementById("Down")
+    let rightButton = document.getElementById("Right")
+    let leftButton = document.getElementById("Left")
+    takeOffButton.addEventListener("click",function(){
+        if(window.confirm(("Confirm that the shuttle is ready for takeoff."))===true){
+        flightStatus.innerHTML= "Shuttle in flight.";
+        }
+    });
+}
+  window.onload = init;
